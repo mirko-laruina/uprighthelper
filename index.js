3d6153c891d78785dbc7e47d6534fd87b3f9b98e
Mirko <mirkoletto@gmail.com>
2024-06-12 22:08:41 +0200

    Adds persistency and play/pause classification

--- index.js
+++ index.js
@@ -5,13 +5,38 @@ var audio = new Audio('audio_file.mp3');
 
 
 async function app() {
-  console.log('Loading mobilenet..');
 
-  // Load the model.
+  const localStorageKey = "classifierData";
+
+  var paused = false;
+
+
+  const loadFromLocalStorage = storageKey => {
+    // Load the model.
+    var dataset = localStorage.getItem(storageKey);
+    if(dataset !== undefined && dataset !== null){
+      console.log("Loading dataset from local storage (" + storageKey + ")");
+      var objects = Object.fromEntries( JSON.parse(dataset).map(([label, data, shape])=>[label, tf.tensor(data, shape)]) )
+      for (const [label, tensor] of Object.entries(objects)){
+        // for each row of the tensor, add it to the classifier
+        for (let i = 0; i < tensor.shape[0]; i++) {
+          classifier.addExample(tensor.slice(i, 1), parseInt(label));
+        }
+      }
+
+      // This should be quicker, but it doesn't work
+      // classifier.setClassifierDataset( Object.fromEntries( JSON.parse(dataset).map(([label, data, shape])=>[label, tf.tensor(data, shape)]) ) );
+      console.log("Successfully loaded dataset from local storage (" + storageKey + ")");
+    }
+  }
+
+  loadFromLocalStorage(localStorageKey);
+  // Setup the webcam before loading the model so that the user can grant the webcam permission while the model is loading.
+  await setupWebcam();
+  console.log('Loading mobilenet..');
   net = await mobilenet.load();
   console.log('Sucessfully loaded model');
 
-  await setupWebcam();
 
   // Reads an image from the webcam and associates it with a specific class
   // index.
@@ -23,14 +48,50 @@ async function app() {
     // Pass the intermediate activation to the classifier.
     classifier.addExample(activation, classId);
     activation.dispose()
+
+    // persist
+    let dataset = JSON.stringify(Object.entries(classifier.getClassifierDataset()).map(([label, data])=>[label, Array.from(data.dataSync()), data.shape]) )
+    localStorage.setItem(localStorageKey, dataset)
   };
+
+  const pause = async () => {
+    if(paused){
+      console.log("Resuming")
+      paused = false;
+    } else {
+      console.log("Pausing")
+      paused = true;
+    }    
+  }
+
+  const clearStorage = async () => {
+    var success = confirm("All data will be lost. Are you sure you want to reset?")
+    if(!success){
+      return;
+    }
+    localStorage.clear();
+    location.reload();
+  }
 
   // When clicking a button, add an example for that class.
   document.getElementById('class-a').addEventListener('click', () => addExample(0));
   document.getElementById('class-b').addEventListener('click', () => addExample(1));
+  document.getElementById('pause').addEventListener('click', (evt) => {
+    pause()
+    evt.target.innerHTML = paused ? "Start" : "Pause"
+  });
+  document.getElementById('reset').addEventListener('click', clearStorage);
 
+
+
+  let firstBadTime = null;
 
   while (true) {
+    await tf.nextFrame()
+    if(paused){
+      continue;
+    }
+
     if (classifier.getNumClasses() > 0) {
       // Get the activation from mobilenet from the webcam.
       const activation = net.infer(webcamElement, 'conv_preds');
@@ -48,8 +109,6 @@ async function app() {
 
       activation.dispose()
     }
-
-    await tf.nextFrame();
   }
 }
 
